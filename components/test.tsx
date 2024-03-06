2ba392f014caf4bd0891a1885d7ce5b4bf94f7cf
Victor Pontis <victor@pont.is>
2024-03-06 11:03:46 -0500

    Update dependencies, add CSS, formatting

--- components/test.tsx
+++ components/test.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as prettier from "prettier/standalone";
+import postcss from "prettier/plugins/postcss";
 
 export const Test = () => {
-  return (
-    <div className="container">
-      <div>Outer should be red</div>
+  const [compiled, setCompiled] = useState("");
 
-      <div className="inner">Inner should be blue</div>
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Pull information from the DOM under the class data-styled-jsx
+      const styles = document.querySelectorAll("style");
+      styles.forEach((style) => {
+        if (style && style.id.startsWith("__jsx-")) {
+          prettier
+            .format(style.innerHTML, {
+              parser: "css",
+              plugins: [postcss],
+            })
+            .then((formatted) => {
+              setCompiled(formatted);
+            });
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div className="outer">
+      <div>
+        This is a test page showing a demo how we can compile nested CSS classes
+        in Styled JSX.
+      </div>
+
+      <div className="container">
+        <div>Outer should be red.</div>
+        <div className="inner">
+          Inner should be blue. Note that you can’t test this on Chrome because
+          Chrome supports CSS nesting built in.
+        </div>
+      </div>
+
+      <div>
+        Here is what the Styled JSX has been compiled to:
+        <pre>{compiled}</pre>
+      </div>
 
       <style jsx>{`
         .container {
@@ -13,10 +50,6 @@ export const Test = () => {
 
           .inner {
             color: blue;
-
-            &:hover {
-              color: green;
-            }
           }
         }
       `}</style>
